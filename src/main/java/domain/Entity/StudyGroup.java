765845c5bd13839735754720d3229b2e2c35daef
minjun <rhalswns0303@naver.com>
2025-05-23 14:10:07 +0900

    Entity StudyGroup

--- src/main/java/domain/Entity/StudyGroup.java
+++ src/main/java/domain/Entity/StudyGroup.java
@@ -8,14 +8,36 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Table(name="StudyGroup")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudyGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="groupId",nullable = false)
     private int groupId;
+
     private String groupName;
+
     private String activityDay;
+
     private String activityTime;
+
     private String topic;
+
+    @Column(name="leaderId", nullable = false)
     private String leaderId;
+
     private int maxMember;
+
     private LocalDate createDate;
+
+
+    @ManyToMany
+    @JoinColumn(name = "leaderId", referencedColumnName = "studentId",
+            foreignKey = @ForeignKey(name = "fk_studygroup_student", foreignKeyDefinition = "FOREIGN KEY (leaderId) REFERENCES student(studentId) ON DELETE CASCADE"))
+    private Student student;
+
 
 }
