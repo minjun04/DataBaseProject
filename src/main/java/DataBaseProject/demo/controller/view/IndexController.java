a3cdf040f9c16778189c76aad7be8e175113833f
minjun <rhalswns0303@naver.com>
2025-06-02 12:20:18 +0900

    ing...

--- src/main/java/DataBaseProject/demo/controller/view/IndexController.java
+++ src/main/java/DataBaseProject/demo/controller/view/IndexController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AuthLoginController {
+public class IndexController {
 
-
-    @GetMapping("/login")
-    public String AuthLogin(){
-        return "login";
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 }
